4b4722238fcd2b4814a9cb684c067a31d73ee115
bruh1555 <128431763+bruh1555@users.noreply.github.com>
2024-08-05 13:33:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print("This is just a test code on module! Nothing more.")
 print("This was only used to test modules for CodeOn.")
+print("You can find this at https://github.com/bruh1555/TestCodeOnModule")
 print("Anyways bye!")
